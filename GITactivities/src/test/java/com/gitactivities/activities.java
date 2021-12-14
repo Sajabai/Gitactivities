be839cf17640d4c3bbbb61d2559c4fc0a98753c7
sudha <sudha@DESKTOP-J4BO6EL>
2021-12-14 11:17:53 +0530

    COMMIT 2

---
+++ GITactivities/src/test/java/com/gitactivities/activities.java
@@ -0,0 +1,44 @@
+package com.gitactivities;
+
+import org.testng.annotations.Test;
+
+public class activities {
+@Test
+	public static void main(String[] args) {
+		
+		@Test
+		public void bcd() {
+				
+				int num1=10;
+				int num2=20;
+				int sum=num1+num2;
+				
+				
+				System.out.println("the sum of num is"+sum);}
+				
+				
+			@Test
+			public void ghfh(){
+					
+					int num1=10;
+					int num2=20;
+					int sum=num1+num2;
+					
+					
+					System.out.println("the sum of num is"+sum);
+					
+					
+					
+					
+				
+			}
+		
+
+	}
+
+private static void bcd() {
+	// TODO Auto-generated method stub
+	
+}
+
+}
